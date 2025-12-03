41465ae28855a869799aa2c1b39140b0c4b1d228
Danielshih <dog830228@gmail.com>
2025-12-03 13:12:11 +0000

    add init script

--- examples/scripts/init_postgres.sql
+++ examples/scripts/init_postgres.sql
@@ -1,31 +1,31 @@
-CREATE TABLE T1 (
+CREATE PUBLICATION cdc_pub FOR ALL TABLES;
+
+CREATE TABLE public.T1 (
     ID SERIAL PRIMARY KEY,
 	val INT NOT NULL,
 	col1 UUID NOT NULL,
 	col2 UUID NOT NULL
 );
 
-INSERT INTO T1
-SELECT i,
-       RANDOM() * 1000000,
-	   md5(random()::text || clock_timestamp()::text)::uuid,
-	   md5(random()::text || clock_timestamp()::text)::uuid
-FROM generate_series(1,1000) i;
+-- INSERT INTO T1
+-- SELECT i,
+--        RANDOM() * 1000000,
+-- 	   md5(random()::text || clock_timestamp()::text)::uuid,
+-- 	   md5(random()::text || clock_timestamp()::text)::uuid
+-- FROM generate_series(1,1000) i;
 
-SELECT
-   format($f$
-      BEGIN;
-      INSERT INTO public.T1 (val, col1, col2)
-      VALUES (
-         RANDOM() * 1000000,
-         md5(random()::text || clock_timestamp()::text)::uuid,
-         md5(random()::text || clock_timestamp()::text)::uuid
-      );
-      COMMIT;
-   $f$)
-FROM generate_series(1, 10000);
-\gexec
+-- SELECT
+--    format($f$
+--       BEGIN;
+--       INSERT INTO public.T1 (val, col1, col2)
+--       VALUES (
+--          RANDOM() * 1000000,
+--          md5(random()::text || clock_timestamp()::text)::uuid,
+--          md5(random()::text || clock_timestamp()::text)::uuid
+--       );
+--       COMMIT;
+--    $f$)
+-- FROM generate_series(1, 10000);
+-- \gexec
 
-CREATE PUBLICATION cdc_pub FOR ALL TABLES;
-
-select pg_drop_replication_slot('cdc_slot');
+--select pg_drop_replication_slot('cdc_slot');
